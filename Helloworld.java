06e833ca9e4d5a01784bf3f159a343ae904cb2aa
Purushotham-N <41068149+Purushotham-N@users.noreply.github.com>
2019-02-22 05:50:02 +0530

    Create Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my first project on Git");
+		System.out.println("Hello world!");
+	}
+
+}
